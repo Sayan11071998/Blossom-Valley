2f6475e342c9c945fa733dfd396736219eb7e7fb
Sayan Nandi <sayannandi11071998@gmail.com>
2025-05-18 02:07:54 +0530

    Player Movement Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float runSpeed = 8f;
+
+    private float moveSpeed;
+
+    private CharacterController controller;
+    private Animator animator;
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 velocity = moveSpeed * Time.deltaTime * dir;
+
+        if (Input.GetButton("Sprint"))
+        {
+            moveSpeed = runSpeed;
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+            animator.SetBool("Running", false);
+        }
+
+        if (dir.magnitude >= 0.1f)
+        {
+            transform.rotation = Quaternion.LookRotation(dir);
+            controller.Move(velocity);
+        }
+
+        animator.SetFloat("Speed", velocity.magnitude);
     }
 }
