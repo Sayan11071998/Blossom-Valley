4ac43574326a3c32b5657a4d8113535c30dcb158
Sayan Nandi <sayannandi11071998@gmail.com>
2025-05-27 02:20:29 +0530

    Adding Logics

--- Assets/Scripts/Buying Selling System/Shop.cs
+++ Assets/Scripts/Buying Selling System/Shop.cs
@@ -1,38 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Shop : InteractableObject
 {
-    public List<ItemData> shopItems;
+    [Header("Shop Items")]
+    [SerializeField] private List<ItemData> shopItems;
 
     [Header("Dialogues")]
-    public List<DialogueLine> dialogueOnShopOpen;
+    [SerializeField] private List<DialogueLine> dialogueOnShopOpen;
 
     public static void Purchase(ItemData item, int quantity)
     {
-        int totalCost = item.cost * quantity; 
+        int totalCost = item.cost * quantity;
         PlayerModel playerModel = FindAnyObjectByType<PlayerView>().PlayerModel;
 
         if (playerModel.Money >= totalCost)
         {
-            // Deduct from the player's money
             playerModel.Spend(totalCost);
-            // Create an ItemSlotData for the purchased item
-            ItemSlotData purchasedItem = new ItemSlotData(item, quantity);
 
-            // Send it to the player's inventory
-            InventoryManager.Instance.ShopToInventory(purchasedItem); 
+            ItemSlotData purchasedItem = new ItemSlotData(item, quantity);
+            InventoryManager.Instance.ShopToInventory(purchasedItem);
         }
     }
 
-    public override void Pickup()
-    {
-        DialogueManager.Instance.StartDialogue(dialogueOnShopOpen, OpenShop);
-    }
+    public override void Pickup() => DialogueManager.Instance.StartDialogue(dialogueOnShopOpen, OpenShop);
 
-    void OpenShop()
-    {
-        UIManager.Instance.OpenShop(shopItems);
-    }
+    private void OpenShop() => UIManager.Instance.OpenShop(shopItems);
 }
