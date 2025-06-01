19e32880498de6383c2e6dd7dfa7f0839dd6ceda
Sayan Nandi <sayannandi11071998@gmail.com>
2025-06-01 17:26:56 +0530

    Wilted Crop Removal Logic Fixed

--- Assets/Scripts/Land System/LandController.cs
+++ Assets/Scripts/Land System/LandController.cs
@@ -118,33 +118,24 @@ namespace BlossomValley.LandSystem
         private void HandleShovelInteraction()
         {
             CropBehaviour cropOnLand = landView.GetComponentInChildren<CropBehaviour>();
-            
+
             if (cropOnLand != null)
             {
-                Debug.Log($"Shovel: Found crop on land {landModel.id}, removing it. Crop state: {cropOnLand.cropState}");
                 landView.RemoveCrop();
                 landModel.SetCropState(false);
                 LandManager.Instance.OnLandStateChange(landModel.id, landModel.landStatus, landModel.timeWatered, landModel.obstacleStatus);
                 return;
             }
-            
+
             if (landModel.hasCrop)
             {
-                Debug.LogWarning($"Shovel: Land {landModel.id} has hasCrop=true but no crop GameObject found. Fixing desync.");
                 landModel.SetCropState(false);
                 LandManager.Instance.OnLandStateChange(landModel.id, landModel.landStatus, landModel.timeWatered, landModel.obstacleStatus);
                 return;
             }
-            
+
             if (landModel.CanRemoveObstacle(LandModel.FarmObstacleStatus.Weeds))
-            {
-                Debug.Log($"Shovel: Removing weeds from land {landModel.id}");
                 SetObstacleStatus(LandModel.FarmObstacleStatus.None);
-            }
-            else
-            {
-                Debug.Log($"Shovel: Nothing to remove on land {landModel.id}. HasCrop: {landModel.hasCrop}, Obstacle: {landModel.obstacleStatus}");
-            }
         }
 
         private void HandleSeedInteraction(SeedData seedTool)
